b811cd71010e3c439f6df31eb3a0f1818e5b1d66
l100320503 <l10032503@gmail.com>
2019-05-12 18:30:07 +0900

    중복제거

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,28 @@ public class Main {
         System.out.println("Hello World!");
         urlList.addAll(danawaPageLink.makePageLinkSelenium());
 
+        List<String> urlList2 = new ArrayList<>();
+        urlList2 = deleteDuplicate(urlList);
+
         System.out.println("====================urllist=============================");
-        System.out.println(urlList);
+        System.out.println(urlList2);
+    }
+
+    public static List<String> deleteDuplicate(List<String> list){
+        List<String> filterlist = new ArrayList<String>();
+        int i = 0;
+
+        for (String element : list) {
+            System.out.println(i++);
+            // If this element is not present in newList
+            // then add it
+            if (!filterlist.contains(element)) {
+
+                filterlist.add(element);
+            }
+        }
+
+        // return the new list
+        return filterlist;
     }
 }
